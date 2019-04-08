0e36eb649f816dff8d0935aef0055e7a5402dc12
zcc <13140433321@163.com>
2019-04-08 20:52:26 +0800

    3

--- src/main/java/cn/itcast/deno/Git.java
+++ src/main/java/cn/itcast/deno/Git.java
@@ -3,6 +3,8 @@ package cn.itcast.deno;
 public class Git {
     public static void main(String[] args) {
 
+
+
         System.out.println("this is git demo");
         System.out.println("this other a error");
         System.out.println("this is a error");
@@ -10,6 +12,8 @@ public class Git {
         System.out.println("nihao");
 
         System.out.println("hello");
+
+        System.out.println("nicai");
 
     }
 }
