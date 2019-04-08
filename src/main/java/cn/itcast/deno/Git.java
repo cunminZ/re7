80f8ed52db7a4ed82e9403da1f5a76d2863fcffa
zcc <13140433321@163.com>
2019-04-08 22:44:34 +0800

    Math.PI

--- src/main/java/cn/itcast/deno/Git.java
+++ src/main/java/cn/itcast/deno/Git.java
@@ -4,18 +4,13 @@ public class Git {
     public static void main(String[] args) {
 
 
-
         System.out.println("this is git demo");
         System.out.println("this other a error");
         System.out.println("this is a error");
 
         System.out.println("nihao");
-
         System.out.println("hello");
-
         System.out.println("nicai");
-
         System.out.println(Math.PI);
-
     }
 }
